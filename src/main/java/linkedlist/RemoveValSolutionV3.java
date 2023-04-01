d938948c560a2163449d8405db387dc306b3bb13
dongliangliang <v_dongliangliang.js@chinatelecom.cn>
2023-04-01 16:02:21 +0800

    author:dongliangliang desc:去除注释

--- src/main/java/linkedlist/RemoveValSolutionV3.java
+++ src/main/java/linkedlist/RemoveValSolutionV3.java
@@ -10,10 +10,6 @@ public class RemoveValSolutionV3 {
     if (head == null) {
       return null;
     }
-
-    System.out.println("head= " + head);
-    System.out.println("head.next= " + head.next);
-    // V1
     ListNode res = removeElements(head.next, val);
 
     if (head.val == val) {
@@ -22,19 +18,9 @@ public class RemoveValSolutionV3 {
       head.next = res;
       return head;
     }
-    // V2
-//    System.out.println("before=====");
-//    System.out.println("head =" + head);
-//    System.out.println("head next=" + head.next);
-//    head.next = removeElements(head.next, val);
-//    System.out.println("after=====");
-//    System.out.println("head=" + head);
-//    System.out.println("head next=" + head.next);
-//    return head.val == val ? head.next : head;
   }
 
   public static void main(String[] args) {
-
     int[] arr = {2, 4, 5, 6, 6, 1, 78};
     ListNode listNode = new ListNode(arr);
     System.out.println(listNode);
