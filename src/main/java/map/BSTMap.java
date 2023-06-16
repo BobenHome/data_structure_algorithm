6ad24cef41dbd168512faccb7ef1c51e4a8bf622
dongliangliang <dongliangliang@dreamdt.cn>
2023-06-16 22:47:26 +0800

    基于二分搜索树的map

---
+++ src/main/java/map/BSTMap.java
@@ -0,0 +1,125 @@
+package map;
+
+/**
+ * @author dongliangliang
+ * @date 2023/6/16 17:34:59
+ */
+public class BSTMap<K extends Comparable<K>, V> implements Map<K, V> {
+
+    private class Node {
+        public K key;
+
+        public V value;
+
+        public Node left, right;
+
+        public Node(K key, V value) {
+            this.key = key;
+            this.value = value;
+            this.left = null;
+            this.right = null;
+        }
+
+        @Override
+        public String toString() {
+            return key.toString() + ": " + value.toString();
+        }
+    }
+
+    private Node root;
+    private int size;
+
+    public BSTMap() {
+        root = null;
+        size = 0;
+    }
+
+    // 向二分搜索树中添加新的元素
+    @Override
+    public void add(K key, V value) {
+        root = add(root, key, value);
+    }
+
+    private Node add(Node node, K key, V value) {
+        // 1.写递归的终止条件
+        if (null == node) {
+            size++;
+            return new Node(key, value);
+        }
+        if (key.compareTo(node.key) < 0) {
+            node.left = add(node.left, key, value);
+        } else if (key.compareTo(node.key) > 0) {
+            node.right = add(node.right, key, value);
+        } else {
+            node.value = value;
+        }
+        return node;
+    }
+
+    @Override
+    public V remove(K key) {
+        return null;
+    }
+
+    @Override
+    public boolean contains(K key) {
+        return getNode(root, key) != null;
+    }
+
+    @Override
+    public V get(K key) {
+        Node node = getNode(root, key);
+        return null == node ? null : node.value;
+    }
+
+    @Override
+    public void set(K key, V newValue) {
+        Node node = getNode(root, key);
+        if (null == node) {
+            throw new IllegalArgumentException(key + " doesn't exist");
+        }
+        node.value = newValue;
+    }
+
+    @Override
+    public int getSize() {
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    // 以node为根节点去寻找key的节点元素
+    private Node getNode(Node node, K key) {
+        if (null == node) {
+            return null;
+        }
+        if (node.key.compareTo(key) < 0) {
+            return getNode(node.left, key);
+        } else if (node.key.compareTo(key) > 0) {
+            return getNode(node.right, key);
+        } else {
+            return node;
+        }
+    }
+
+    private Node minimum(Node node) {
+        if (node.left == null) {
+            return node;
+        }
+        return minimum(node.left);
+    }
+
+    private Node removeMin(Node node) {
+        if (node.left == null) {
+            Node rightNode = node.right;
+            size--;
+            node.right = null;
+            return rightNode;
+        }
+        node.left = removeMin(node.left);
+        return node;
+    }
+}
